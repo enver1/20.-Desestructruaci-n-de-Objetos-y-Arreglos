d3c5a012c549e2c299f2e383674500f17ebd3db9
enver1 <enver.flores.2036@gmail.com>
2023-11-09 16:05:04 -0500

    25, 26. Constructores de una clase en TypeScript
    crear propiedades
    valores opcionales
    valores por defecto

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 "use strict";
 (() => {
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la mision`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la mision`);
-    };
-    const wolverine = {
-        nombre: 'logan',
-        edad: 60
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
+    //opercamelcase 1er mayuscula
+    //lowercamelcase 1er minuscula
+    // class Avenger {
+    //     nombre: string;
+    //     equipo: string;
+    //     nombreReal?: string;
+    //     puedePelear?: boolean;
+    //     peleasGanadas?: number;
+    //     constructor( nombre: string, equipo: string ){
+    //     this.nombre = nombre;
+    //     this.equipo = equipo;
+    //     }
+    //   }
+    class Avenger {
+        constructor(nombre, equipo, nombreReal, puedePelear, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    const antman = new Avenger('Atman', 'capi');
+    console.log(antman);
 })();
