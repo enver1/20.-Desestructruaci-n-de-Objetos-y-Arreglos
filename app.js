2ab46d2d74d7a70c50569aa82db5bbbb288e9494
enver1 <enver.flores.2036@gmail.com>
2023-11-09 13:31:00 -0500

    21. Promesas
    resolve
    reject

--- app.js
+++ app.js
@@ -1,55 +1,17 @@
 "use strict";
 (() => {
-    const avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan america',
-        poder: 'Droga'
-    };
-    const extraer = ({ nombre, poder }) => {
-        console.log(nombre);
-        console.log(poder);
-    };
-    extraer(avenger);
-    // //ejemplo 1
-    // const extraer = ( avenger: any ) => {
-    //     const {nombre, clave, poder } = avenger;
-    //     console.log( nombre );
-    //     console.log( poder );
-    // }
-    // extraer( avenger );
-    // const {nombre, clave, poder } = avenger;
-    // console.log( nombre );
-    // console.log( clave);
-    // console.log( avenger.nombre);
-    // console.log( avenger.clave);
-    // console.log( avenger.poder);
-    // // ejercicio 2
-    // const avengers: string [] = ['Thor', 'Iroman', 'Spiderman'];
-    // console.log (avengers[0]); 
-    // //desestructuracion ej 3
-    // const avengers: string [] = ['Thor', 'Iroman', 'Spiderman'];
-    // const [ loki, hombre, arana ] = avengers;
-    // console.log ( loki );
-    // console.log ( hombre );
-    // console.log ( arana);
-    //  //desestructuracion ej 4
-    //  const avengers: string [] = ['Thor', 'Iroman', 'Spiderman'];
-    //  const [  ,  , arana ] = avengers;
-    //  console.log ( arana);
-    //  // ej 5
-    // const avengers: string [] = ['Thor', 'Iroman', 'Spiderman'];
-    // const extraerArr = ( avengers: string[] ) => {  
-    // console.log( avengers[0] );
-    // console.log( avengers[1] );
-    // console.log( avengers[2] );
-    // }      
-    // extraerArr( avengers ) ;
-    // ej 6
-    const avengers = ['Thor', 'Iroman', 'Spiderman'];
-    const extraerArr = ([thor3, iroman3, spiderman3]) => {
-        console.log(thor3);
-        console.log(iroman3);
-        console.log(spiderman3);
-    };
-    extraerArr(avengers);
+    console.log('inicio');
+    const promesa = new Promise((resolve, reject) => {
+        // setTimeout( () => {
+        // resolve('Se termino el timeout');
+        // }, 1000);
+        // });
+        setTimeout(() => {
+            reject('Se termino el timeout');
+        }, 1000);
+    });
+    promesa
+        .then(mensaje => console.log(mensaje))
+        .catch(err => console.warn(err));
+    console.log('fin');
 })();
