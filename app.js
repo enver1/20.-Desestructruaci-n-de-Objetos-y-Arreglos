c62cba4fa97d4672ceeaf64ccef610b3c41b7112
enver1 <enver.flores.2036@gmail.com>
2023-11-13 10:24:10 -0500

    Reproducir 30. Tipado del retorno de una función
    tipos  retorno de un función

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 "use strict";
 (() => {
-    //opercamelcase 1er mayuscula
-    //lowercamelcase 1er minuscula
-    // class Avenger {
-    //     nombre: string;
-    //     equipo: string;
-    //     nombreReal?: string;
-    //     puedePelear?: boolean;
-    //     peleasGanadas?: number;
-    //     constructor( nombre: string, equipo: string ){
-    //     this.nombre = nombre;
-    //     this.equipo = equipo;
-    //     }
-    //   }
-    class Avenger {
-        constructor(nombre, equipo, nombreReal, puedePelear, peleasGanadas = 0) {
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
-        }
-    }
-    const antman = new Avenger('Atman', 'capi');
-    console.log(antman);
+    const sumar = (a, b) => {
+        return a + b;
+    };
+    const combre = () => {
+        return 'Hola';
+    };
+    const obtenerSalario = () => {
+        return new Promise((resolve, reject) => {
+            resolve('Fernando');
+        });
+    };
+    obtenerSalario().then(a => console.log(a.toUpperCase()));
 })();
